9bc68aebcbbbf2b4fba96867e685737fe0017a9f
IgorWalkowiak <IgorWalkowiak@protonmail.com>
2018-04-28 10:20:36 +0200

    Closed project

--- Tree.h
+++ Tree.h
@@ -3,29 +3,27 @@
 #include<iostream>
 
 template < typename KeyType, typename ValueType >
-
 struct node
     {
-        node(KeyType K,ValueType V):key(K),value(V),left(nullptr),right(nullptr){};
+    node(KeyType K,ValueType V):key(K),value(V),left(nullptr),right(nullptr){};
 
-        KeyType key;
-        ValueType value;
+    KeyType key;
+    ValueType value;
 
-        node* left;
-        node* right;
-
-
+    node* left;
+    node* right;
     };
 
 
 
 template < typename KeyType, typename ValueType >
-
 class Tree{
 
     node<KeyType,ValueType>* root;
     void addNode(node<KeyType,ValueType>*);
     void showNode(node<KeyType,ValueType>*);
+    void removeNode(node<KeyType,ValueType>*);
+    node<KeyType,ValueType>* lastLeftNode(node<KeyType,ValueType>*);
 
     KeyType keyBuf;
     ValueType valueBuf;
@@ -37,6 +35,7 @@ public:
     void show(void);
 
 };
+
 
 
 template <typename KeyType, typename ValueType>
@@ -52,10 +51,87 @@ void Tree<KeyType,ValueType>::add(KeyType keyIN, ValueType valueIN)
     }
 }
 
+
+
 template <typename KeyType, typename ValueType>
 void Tree<KeyType,ValueType>::remove(KeyType keyIN)
 {
+    keyBuf=keyIN;
+    if(root==nullptr) std::cout<<"Something goes wrong";
+    else if(root->key==keyBuf)
+    {
+            node<KeyType,ValueType>* leftNodeBuf=root->left;
+            node<KeyType,ValueType>* rightNodeBuf=root->right;
+            delete root;
+            root=rightNodeBuf;
+                if(root!=nullptr)
+                {
+                    lastLeftNode(root)->left=leftNodeBuf;
+                }
+                else root=leftNodeBuf;
+    }
+    else
+    removeNode(root);
+}
 
+
+
+template <typename KeyType, typename ValueType>
+void Tree<KeyType,ValueType>::removeNode(node<KeyType,ValueType>* fatherNode)
+{
+    if(fatherNode->key>keyBuf)//left
+    {
+        if(fatherNode->left!=nullptr)
+        {
+            if(fatherNode->left->key==keyBuf)
+            {
+                node<KeyType,ValueType>* leftNodeBuf=fatherNode->left->left;
+                node<KeyType,ValueType>* rightNodeBuf=fatherNode->left->right;
+                delete fatherNode->left;
+                fatherNode->left=rightNodeBuf;
+                if(fatherNode->left!=nullptr)
+                {
+                    lastLeftNode(fatherNode->left)->left=leftNodeBuf;
+                }
+                else fatherNode=leftNodeBuf;
+            }
+            else
+            removeNode(fatherNode->left);
+        }
+        else
+        std::cout<<"There is no key: "<<keyBuf;
+    }
+    else //right
+    {
+        if(fatherNode->right!=nullptr)
+        {
+            if(fatherNode->right->key==keyBuf)
+            {
+                node<KeyType,ValueType>* leftNodeBuf=fatherNode->right->left;
+                node<KeyType,ValueType>* rightNodeBuf=fatherNode->right->right;
+                delete fatherNode->right;
+                fatherNode->right=rightNodeBuf;
+                if(fatherNode->right!=nullptr)
+                {
+                    lastLeftNode(fatherNode->right)->left=leftNodeBuf;
+                }
+                else fatherNode=leftNodeBuf;
+            }
+            else
+            removeNode(fatherNode->right);
+        }
+        else
+        std::cout<<"There is no key: "<<keyBuf;
+    }
+}
+
+
+template <typename KeyType, typename ValueType>
+node<KeyType,ValueType>* Tree<KeyType,ValueType>::lastLeftNode(node<KeyType,ValueType>* fatherNode)
+{
+    if(fatherNode->left!=nullptr)
+    lastLeftNode(fatherNode->left);
+    else return fatherNode;
 }
 
 
@@ -77,13 +153,13 @@ void Tree<KeyType,ValueType>::addNode(node<KeyType,ValueType>* fatherNode)
 {
     if(fatherNode->key>keyBuf) //left node
     {
-
         if(fatherNode->left==nullptr)
         {
             fatherNode->left=new node<KeyType,ValueType>(keyBuf,valueBuf);
         }
         else addNode(fatherNode->left);
     }
+
     else if(fatherNode->key<keyBuf) //right node
     {
         if(fatherNode->right==nullptr)
@@ -99,14 +175,13 @@ void Tree<KeyType,ValueType>::addNode(node<KeyType,ValueType>* fatherNode)
 }
 
 
+
 template <typename KeyType, typename ValueType>
 void Tree<KeyType,ValueType>::showNode(node<KeyType,ValueType>* fatherNode)
 {
     if(fatherNode->left!=nullptr) showNode(fatherNode->left);
+      std::cout<<"Key: "<<fatherNode->key<<" Value: "<<fatherNode->value<<std::endl;
     if(fatherNode->right!=nullptr) showNode(fatherNode->right);
-
-    std::cout<<fatherNode->key<<" "<<fatherNode->value<<std::endl;
-
 }
 
 
